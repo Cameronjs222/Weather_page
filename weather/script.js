fd260b9179a58ec247897606e265019e899c816c
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-01-27 02:00:54 -0800

    add js file and made temp change onchange

--- weather/script.js
+++ weather/script.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+function change(){
+    let C = document.getElementsByClassName("Celcius");
+    let F = document.getElementsByClassName("Fahrenheit");
+    let val = document.querySelector(`#temp`).value
+    if (val === "Fahrenheit") {
+        for (let i = 0; i < C.length; i++) {
+            C[i].style.display = "none";
+        }
+        for (let i = 0; i < F.length; i++) {
+            F[i].style.display = "block"
+        }
+    }
+    else if (val === "Celcius") {
+        for (let i = 0; i < F.length; i++) {
+            F[i].style.display = "none"
+        }
+        for (let i = 0; i < C.length; i++) {
+            C[i].style.display = "block";
+        }
+    }
+    console.log(val)
+}
